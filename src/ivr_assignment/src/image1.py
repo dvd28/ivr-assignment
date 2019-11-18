d04f4ca231b63e66099795417bb5900945a45719
jerryzhao173985 <44931279+jerryzhao173985@users.noreply.github.com>
2019-11-18 19:54:55 +0000

    Add files via upload

--- src/ivr_assignment/src/image1.py
+++ src/ivr_assignment/src/image1.py
@@ -25,6 +25,94 @@ class image_converter:
     self.bridge = CvBridge()
 
 
+
+
+#----------------------------------
+def detect_red1(self,image):
+      # Isolate the blue colour in the image as a binary image
+      mask = cv2.inRange(image, (0, 0, 100), (0, 0, 255))
+      # This applies a dilate that makes the binary region larger (the more iterations the larger it becomes)
+      kernel = np.ones((5, 5), np.uint8)
+      mask = cv2.dilate(mask, kernel, iterations=3)
+      # Obtain the moments of the binary image
+      M = cv2.moments(mask)
+      # Calculate pixel coordinates for the centre of the blob
+      cz = int(M['m10'] / M['m00'])
+      cy = int(M['m01'] / M['m00'])
+      return np.array([cz, cy])
+ 
+
+  # Detecting the centre of the green circle
+  def detect_green1(self,image):
+      mask = cv2.inRange(image, (0, 100, 0), (0, 255, 0))
+      kernel = np.ones((5, 5), np.uint8)
+      mask = cv2.dilate(mask, kernel, iterations=3)
+      M = cv2.moments(mask)
+      cz = int(M['m10'] / M['m00'])
+      cy = int(M['m01'] / M['m00'])
+      return np.array([cz, cy])
+
+
+  # Detecting the centre of the blue circle
+  def detect_blue1(self,image):
+      mask = cv2.inRange(image, (100, 0, 0), (255, 0, 0))
+      kernel = np.ones((5, 5), np.uint8)
+      mask = cv2.dilate(mask, kernel, iterations=3)
+      M = cv2.moments(mask)
+      cz = int(M['m10'] / M['m00'])
+      cy = int(M['m01'] / M['m00'])
+      return np.array([cz, cy])
+
+  # Detecting the centre of the yellow circle
+  def detect_yellow1(self,image):
+      mask = cv2.inRange(image, (0, 100, 100), (0, 255, 255))
+      kernel = np.ones((5, 5), np.uint8)
+      mask = cv2.dilate(mask, kernel, iterations=3)
+      M = cv2.moments(mask)
+      cz = int(M['m10'] / M['m00'])
+      cy = int(M['m01'] / M['m00'])
+      return np.array([cz, cy])
+
+
+  # Calculate the conversion from pixel to meter
+  def pixel2meter1(self,image):
+      # Obtain the centre of each coloured blob
+      circle1Pos = self.detect_blue(image)
+      circle2Pos = self.detect_green(image)
+      # find the distance between two circles
+      dist = np.sum((circle1Pos - circle2Pos)**2)
+      return 3 / np.sqrt(dist)
+
+
+    # Calculate the relevant joint angles from the image
+  def detect_joint_angle1(self,image):
+    a = self.pixel2meter(image)
+    # Obtain the centre of each coloured blob 
+    center = a * self.detect_yellow(image)
+    circle1Pos = a * self.detect_blue(image) 
+    circle2Pos = a * self.detect_green(image) 
+    circle3Pos = a * self.detect_red(image)
+    # Solve using trigonometry
+    ja1 = np.arctan2(center[0]- circle1Pos[0], center[1] - circle1Pos[1])
+    ja2 = np.arctan2(circle1Pos[0]-circle2Pos[0], circle1Pos[1]-circle2Pos[1]) - ja1
+    ja3 = np.arctan2(circle2Pos[0]-circle3Pos[0], circle2Pos[1]-circle3Pos[1]) - ja2 - ja1
+    return np.array([ja1, ja2, ja3])
+  
+  #--------------------------------------------
+def detect_joint_position1(self,image):
+    a = self.pixel2meter(image)
+    # Obtain the centre of each coloured blob 
+    center = a * self.detect_yellow(image)
+    circle1Pos = a * self.detect_blue(image) 
+    circle2Pos = a * self.detect_green(image) 
+    circle3Pos = a * self.detect_red(image)
+
+    # make joint position into 2D array(Position array)
+    return np.array([[0.0,center[1],center[0]],[0.0,circle1Pos[1],circle1Pos[0]],[0.0,circle2Pos[1],circle2Pos[0]],[0.0,circle3Pos[1],circle3Pos[0]] ])
+
+
+
+
   # Recieve data from camera 1, process it, and publish
   def callback1(self,data):
     # Recieve the image
@@ -35,12 +123,24 @@ class image_converter:
     
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
+    a = self.detect_joint_angles(cv_image1)
+    b = self.detect_joint_position(cv_image1)
 
     im1=cv2.imshow('window1', self.cv_image1)
     cv2.waitKey(1)
+    
+    self.joints1 = Float64MultiArray()
+    self.joints1.data = a 
+    self.positions1 = Float64MultiArray()
+    self.positions1.data = b
+
+
     # Publish the results
     try: 
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+      self.joints_pub1.publish(self.joints1)
+      self.joints_pub1.publish(self.positions1)
+
     except CvBridgeError as e:
       print(e)
 
--- src/ivr_assignment/src/image2.py
+++ src/ivr_assignment/src/image2.py
@@ -25,6 +25,90 @@ class image_converter:
     self.bridge = CvBridge()
 
 
+#----------------------------------
+def detect_red(self,image):
+      # Isolate the blue colour in the image as a binary image
+      mask = cv2.inRange(image, (0, 0, 100), (0, 0, 255))
+      # This applies a dilate that makes the binary region larger (the more iterations the larger it becomes)
+      kernel = np.ones((5, 5), np.uint8)
+      mask = cv2.dilate(mask, kernel, iterations=3)
+      # Obtain the moments of the binary image
+      M = cv2.moments(mask)
+      # Calculate pixel coordinates for the centre of the blob
+      cx = int(M['m10'] / M['m00'])
+      cz = int(M['m01'] / M['m00'])
+      return np.array([cx, cz])
+ 
+
+  # Detecting the centre of the green circle
+  def detect_green(self,image):
+      mask = cv2.inRange(image, (0, 100, 0), (0, 255, 0))
+      kernel = np.ones((5, 5), np.uint8)
+      mask = cv2.dilate(mask, kernel, iterations=3)
+      M = cv2.moments(mask)
+      cx = int(M['m10'] / M['m00'])
+      cz = int(M['m01'] / M['m00'])
+      return np.array([cx, cz])
+
+
+  # Detecting the centre of the blue circle
+  def detect_blue(self,image):
+      mask = cv2.inRange(image, (100, 0, 0), (255, 0, 0))
+      kernel = np.ones((5, 5), np.uint8)
+      mask = cv2.dilate(mask, kernel, iterations=3)
+      M = cv2.moments(mask)
+      cx = int(M['m10'] / M['m00'])
+      cz = int(M['m01'] / M['m00'])
+      return np.array([cx, cz])
+
+  # Detecting the centre of the yellow circle
+  def detect_yellow(self,image):
+      mask = cv2.inRange(image, (0, 100, 100), (0, 255, 255))
+      kernel = np.ones((5, 5), np.uint8)
+      mask = cv2.dilate(mask, kernel, iterations=3)
+      M = cv2.moments(mask)
+      cx = int(M['m10'] / M['m00'])
+      cz = int(M['m01'] / M['m00'])
+      return np.array([cx, cz])
+
+
+  # Calculate the conversion from pixel to meter
+  def pixel2meter(self,image):
+      # Obtain the centre of each coloured blob
+      circle1Pos = self.detect_blue(image)
+      circle2Pos = self.detect_green(image)
+      # find the distance between two circles
+      dist = np.sum((circle1Pos - circle2Pos)**2)
+      return 3 / np.sqrt(dist)
+
+
+    # Calculate the relevant joint angles from the image
+  def detect_joint_angles(self,image):
+    a = self.pixel2meter(image)
+    # Obtain the centre of each coloured blob 
+    center = a * self.detect_yellow(image)
+    circle1Pos = a * self.detect_blue(image) 
+    circle2Pos = a * self.detect_green(image) 
+    circle3Pos = a * self.detect_red(image)
+    # Solve using trigonometry
+    ja1 = np.arctan2(center[0]- circle1Pos[0], center[1] - circle1Pos[1])
+    ja2 = np.arctan2(circle1Pos[0]-circle2Pos[0], circle1Pos[1]-circle2Pos[1]) - ja1
+    ja3 = np.arctan2(circle2Pos[0]-circle3Pos[0], circle2Pos[1]-circle3Pos[1]) - ja2 - ja1
+    return np.array([ja1, ja2, ja3])
+  
+  #--------------------------------------------
+def detect_joint_position(self,image):
+    a = self.pixel2meter(image)
+    # Obtain the centre of each coloured blob 
+    center = a * self.detect_yellow(image)
+    circle1Pos = a * self.detect_blue(image) 
+    circle2Pos = a * self.detect_green(image) 
+    circle3Pos = a * self.detect_red(image)
+
+    # make joint position into 2D array(Position array)
+    return np.array([[center[0],0.0,center[1]],[circle1Pos[0],0.0,circle1Pos[1]],[circle2Pos[0],0.0,circle2Pos[1]],[circle3Pos[0],0.0,circle3Pos[1]] ])
+
+
   # Recieve data, process it, and publish
   def callback2(self,data):
     # Recieve the image
@@ -34,12 +118,24 @@ class image_converter:
       print(e)
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
+    c = self.detect_joint_angles(cv_image2)
+    d = self.detect_joint_position(cv_image2)
+
     im2=cv2.imshow('window2', self.cv_image2)
     cv2.waitKey(1)
+
+    self.joints2 = Float64MultiArray()
+    self.joints2.data = c
+    self.positions2 = Float64MultiArray()
+    self.positions2.data = d
+
 
     # Publish the results
     try: 
       self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
+      self.joints_pub2.publish(self.joints2)
+      self.joints_pub2.publish(self.positions2)
+
     except CvBridgeError as e:
       print(e)
 
