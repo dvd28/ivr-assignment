47aea055bd2846ed1ace2d9c229debb1fb0cc7bd
David Nguyen <davidngn28@gmail.com>
2019-11-26 13:44:27 +0000

    uploaded the code for target tracking

--- src/ivr_assignment/src/image1.py
+++ src/ivr_assignment/src/image1.py
@@ -21,11 +21,26 @@ class image_converter:
     # initialize a publisher to send images from camera1 to a topic named image_topic1
     self.image_pub1 = rospy.Publisher("image_topic1",Image, queue_size = 1)
     self.positions_pub1 = rospy.Publisher("joints_positions1",Float64MultiArray, queue_size = 10)
+
+    # initialise a publishers for sending the prediction of y and z coordinates of target
+    self.target_ypredict_pub = rospy.Publisher("target/ypredict", Float64, queue_size=1)
+    self.target_zpredict_pub = rospy.Publisher("target/zpredict1", Float64, queue_size=1)
+
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
     # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
     self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
    
+    # a few things to remember
+    self.callback_called = False
+    self.pixel_meter_ratio = 0
+    self.yellow_img_xypos = np.array([-1, -1])
+    self.blue_img_xypos = np.array([-1, -1])
+    self.target_yhistory = []  # remember predicted coordinates of the target
+    self.target_zhistory = [] 
+
+    # load target template
+    self.target_templ = cv2.inRange(cv2.imread('circle1.png', 1), (200, 200, 200), (255, 255, 255))
 
 
 #----------------------------------
@@ -116,15 +131,136 @@ class image_converter:
     return np.array([[0.0,yellow[0],yellow[1]],[0.0,blue[0],blue[1]],[0.0,green[0],green[1]],[0.0,red[0],red[1]] ])
 
 
+  # Return y-z coordinates of the target (in m) with respect to the yellow joint / base frame
+  def detect_target(self, image):
+    # detect orange
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_orange = np.array([15, 20, 20])
+    upper_orange = np.array([25, 255, 255])
+    image_orange = cv2.inRange(image_hsv, lower_orange, upper_orange)
+
+    # detect the window to search in
+
+    # find leftmost white pixel's column index
+    leftmost_pos = 0
+    while not np.any(image_orange[:, leftmost_pos]):
+      leftmost_pos += 1
+
+    # find rightmost white pixel's column index
+    rightmost_pos = image_orange.shape[1] - 1 
+    while not np.any(image_orange[:, rightmost_pos]):
+      rightmost_pos -= 1
+
+    # find the uppermost white pixel's row index
+    uppermost_pos = 0
+    while not np.any(image_orange[uppermost_pos, :]):
+      uppermost_pos += 1
+
+    # find the lowermost white pixel's row index
+    lowermost_pos = image_orange.shape[0] - 1
+    while not np.any(image_orange[lowermost_pos, :]):
+      lowermost_pos -= 1
+    
+    window = image_orange[uppermost_pos:(lowermost_pos + 1), 
+                          leftmost_pos:(rightmost_pos + 1)].copy()
+    window = np.pad(window, [(self.target_templ.shape[0], ), (self.target_templ.shape[1], )],
+                    'constant')
+
+    # do chamfer matching in the window, find position of target relative to the window
+    window_dt = cv2.distanceTransform(cv2.bitwise_not(window), cv2.DIST_L2, 0)
+    chamfer_sums = np.zeros((window_dt.shape[0] - self.target_templ.shape[0] + 1,
+                            window_dt.shape[1] - self.target_templ.shape[1] + 1))
+    for row in range(window_dt.shape[0] - self.target_templ.shape[0] + 1):
+      for column in range(window_dt.shape[1] - self.target_templ.shape[1] + 1):
+        chamfer_sums[row, column] = np.sum(
+          window_dt[row:(row + self.target_templ.shape[0]),
+          column:(column + self.target_templ.shape[1])] * self.target_templ)
+    
+    chamfmin_corner_row, chamfmin_corner_col = np.unravel_index(np.argmin(chamfer_sums), 
+                          (chamfer_sums.shape[0], chamfer_sums.shape[1]))
+    chamfmin_center_row = chamfmin_corner_row + (self.target_templ.shape[0] - 1) // 2
+    chamfmin_center_col = chamfmin_corner_col + (self.target_templ.shape[1] - 1) // 2
+
+    # find (y,z) position of target relative to the base frame / yellow joint
+    window_corner_row = uppermost_pos - self.target_templ.shape[0]
+    window_corner_col = leftmost_pos - self.target_templ.shape[1]
+    chamfmin_img_row = window_corner_row + chamfmin_center_row
+    chamfmin_img_col = window_corner_col + chamfmin_center_col
+    ypredict_p = chamfmin_img_col - self.yellow_img_xypos[0] 
+    zpredict_p = self.yellow_img_xypos[1] - chamfmin_img_row
+    ypredict_m = ypredict_p/self.pixel_meter_ratio
+    zpredict_m = zpredict_p/self.pixel_meter_ratio
+    
+    
+    # check if the estimated position makes sense (deal with occlusion)
+    # use the predicted y position as an indicator of tracking the incorrect object
+    if len(self.target_yhistory) == 2:
+
+      if abs(ypredict_m - self.target_yhistory[0]) > 1:
+        ypredict_m = self.target_yhistory[0] + \
+                     (self.target_yhistory[0] - self.target_yhistory[1])
+        zpredict_m = self.target_zhistory[0] + \
+                     (self.target_zhistory[0] - self.target_zhistory[1])
+        ypredict_p = int(np.around(ypredict_m * self.pixel_meter_ratio))
+        zpredict_p = int(np.around(zpredict_m * self.pixel_meter_ratio))
+
+      self.target_yhistory.insert(0, ypredict_m)
+      self.target_zhistory.insert(0, zpredict_m)
+      self.target_yhistory.pop()
+      self.target_zhistory.pop()
+
+    else:
+      self.target_yhistory.insert(0, ypredict_m)
+      self.target_zhistory.insert(0, zpredict_m)
+    
+   
+    #uncomment to show tracking of target
+    '''
+    chamfmin_img_row = self.yellow_img_xypos[1] - zpredict_p
+    chamfmin_img_col = self.yellow_img_xypos[0] + ypredict_p
+    image_orange[(chamfmin_img_row-30):(chamfmin_img_row+30), chamfmin_img_col] = 100
+    image_orange[chamfmin_img_row, (chamfmin_img_col-30):(chamfmin_img_col+30)] = 100
+    im1=cv2.imshow('window1', image_orange)
+    cv2.waitKey(1)
+    '''
+
+
+    return np.array([ypredict_m, zpredict_m]) 
+
+
 
 
   # Recieve data from camera 1, process it, and publish
   def callback1(self,data):
+
+    # first time the node is run callback receives a faulty image from the camera
+    if not self.callback_called:
+      self.callback_called = True
+      return
+   
+
     # Recieve the image
     try:
       cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
+    
+
+    # Calculate the position of the yellow joint in the image once
+    if np.all(self.yellow_img_xypos == np.array([-1, -1])):
+      self.yellow_img_xypos = self.detect_yellow1(cv_image1)
+    # Calculate the position of the blue joint in the image once
+    if np.all(self.blue_img_xypos == np.array([-1, -1])):
+      self.blue_img_xypos = self.detect_blue1(cv_image1)
+
+    # Calculate the pixel/meter ratio once
+    if self.pixel_meter_ratio == 0:
+      self.pixel_meter_ratio = np.linalg.norm(self.yellow_img_xypos - self.blue_img_xypos)/2
+      print(self.pixel_meter_ratio)  
+ 
+
+    # Detect the target's (y, z) position
+    target_yzposition = self.detect_target(cv_image1)
 
 
 
@@ -164,16 +300,18 @@ class image_converter:
     image_test = image_test1 + image_test2
     cv2.imwrite('red2.png', image_test)
     '''
-    #cv2.imwrite('test_image.png', cv_image1)
+
+
 
 
     # Uncomment if you want to save the image
-    #cv2.imwrite('image_copy.png', cv_image)
+    #cv2.imwrite('image_copy.png', cv_image1)
     
 #-----------------FALUTY Z AXIS(inverse it)---------------
 
 #---------------------------------------------
 
+    # Uncomment to show the feed from the camera
     #im1=cv2.imshow('window1',cv_image1)
     #cv2.waitKey(3)
 
@@ -191,6 +329,9 @@ class image_converter:
       self.positions_pub1.publish(self.positions1) 
       #print(self.positions1.data)
 
+      # publish estimated y-z coordinate of the target     
+      self.target_ypredict_pub.publish(target_yzposition[0])
+      self.target_zpredict_pub.publish(target_yzposition[1])
 
       with open('positions1.txt', 'wb') as fp:
         pickle.dump(self.positions1, fp)
--- src/ivr_assignment/src/image2.py
+++ src/ivr_assignment/src/image2.py
@@ -21,10 +21,28 @@ class image_converter:
     # initialize a publisher to send images from camera2 to a topic named image_topic2
     self.image_pub2 = rospy.Publisher("image_topic2",Image, queue_size = 1)
     self.positions_pub2 = rospy.Publisher("joints_positions2",Float64MultiArray, queue_size = 10)
+
+    # initialise a publishers for sending the prediction of the x coordinate of target
+    self.target_xpredict_pub = rospy.Publisher("target/xpredict", Float64, queue_size=1)
+    self.target_zpredict2_pub = rospy.Publisher("target/zpredict2", Float64, queue_size=1)
+
+
     # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
     self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
+
+    # a few things to remember
+    self.callback_called = False;
+    self.pixel_meter_ratio = 0;
+    self.yellow_img_xypos = np.array([-1, -1])  # yellow and blue joints are fixed in space
+    self.blue_img_xypos = np.array([-1, -1])
+    self.target_xhistory = []  # remember predicted coordinates of the target
+    self.target_zhistory = [] 
+
+    # load target template
+    self.target_templ = cv2.inRange(cv2.imread('circle2.png', 1), (200, 200, 200), (255, 255, 255))
+ 
 
 
 #----------------------------------
@@ -114,8 +132,115 @@ class image_converter:
     return np.array([[yellow[0],0.0,yellow[1]],[blue[0],0.0,blue[1]],[green[0],0.0,green[1]],[red[0],0.0,red[1]] ])
 
 
+
+
+  # Return x-z coordinates of the target with respect to the yellow joint / base frame
+  def detect_target(self, image):
+    # detect orange
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_orange = np.array([15, 20, 20])
+    upper_orange = np.array([25, 255, 255])
+    image_orange = cv2.inRange(image_hsv, lower_orange, upper_orange)
+
+
+    # detect the window to search in
+
+    # find leftmost white pixel's column index
+    leftmost_pos = 0
+    while not np.any(image_orange[:, leftmost_pos]):
+      leftmost_pos += 1
+
+    # find rightmost white pixel's column index
+    rightmost_pos = image_orange.shape[1] - 1 
+    while not np.any(image_orange[:, rightmost_pos]):
+      rightmost_pos -= 1
+
+    # find the uppermost white pixel's row index
+    uppermost_pos = 0
+    while not np.any(image_orange[uppermost_pos, :]):
+      uppermost_pos += 1
+
+    # find the lowermost white pixel's row index
+    lowermost_pos = image_orange.shape[0] - 1
+    while not np.any(image_orange[lowermost_pos, :]):
+      lowermost_pos -= 1
+    
+    window = image_orange[uppermost_pos:(lowermost_pos + 1), 
+                          leftmost_pos:(rightmost_pos + 1)].copy()
+    window = np.pad(window, [(self.target_templ.shape[0], ), (self.target_templ.shape[1], )],
+                    'constant')
+
+    # do chamfer matching in the window, find position of target relative to the window
+    window_dt = cv2.distanceTransform(cv2.bitwise_not(window), cv2.DIST_L2, 0)
+    chamfer_sums = np.zeros((window_dt.shape[0] - self.target_templ.shape[0] + 1,
+                            window_dt.shape[1] - self.target_templ.shape[1] + 1))
+    for row in range(window_dt.shape[0] - self.target_templ.shape[0] + 1):
+      for column in range(window_dt.shape[1] - self.target_templ.shape[1] + 1):
+        chamfer_sums[row, column] = np.sum(
+          window_dt[row:(row + self.target_templ.shape[0]),
+          column:(column + self.target_templ.shape[1])] * self.target_templ)
+    
+    chamfmin_corner_row, chamfmin_corner_col = np.unravel_index(np.argmin(chamfer_sums), 
+                          (chamfer_sums.shape[0], chamfer_sums.shape[1]))
+    chamfmin_center_row = chamfmin_corner_row + (self.target_templ.shape[0] - 1) // 2
+    chamfmin_center_col = chamfmin_corner_col + (self.target_templ.shape[1] - 1) // 2
+
+    # find position of target relative to the base frame / yellow joint
+    window_corner_row = uppermost_pos - self.target_templ.shape[0]
+    window_corner_col = leftmost_pos - self.target_templ.shape[1]
+    chamfmin_img_row = window_corner_row + chamfmin_center_row
+    chamfmin_img_col = window_corner_col + chamfmin_center_col
+    xpredict_p = chamfmin_img_col - self.yellow_img_xypos[0] 
+    zpredict_p = self.yellow_img_xypos[1] - chamfmin_img_row
+    xpredict_m = xpredict_p/self.pixel_meter_ratio
+    zpredict_m = zpredict_p/self.pixel_meter_ratio
+    
+    
+    # check if the estimated position makes sense (deal with occlusion)
+    # use the predicted x position as an indicator of tracking the incorrect object
+    if len(self.target_xhistory) == 2:
+
+      if abs(xpredict_m - self.target_xhistory[0]) > 1:
+        xpredict_m = self.target_xhistory[0] + \
+                     (self.target_xhistory[0] - self.target_xhistory[1])
+        zpredict_m = self.target_zhistory[0] + \
+                     (self.target_zhistory[0] - self.target_zhistory[1])
+        xpredict_p = int(np.around(xpredict_m * self.pixel_meter_ratio))
+        zpredict_p = int(np.around(zpredict_m * self.pixel_meter_ratio))
+
+      self.target_xhistory.insert(0, xpredict_m)
+      self.target_zhistory.insert(0, zpredict_m)
+      self.target_xhistory.pop()
+      self.target_zhistory.pop()
+
+    else:
+      self.target_xhistory.insert(0, xpredict_m)
+      self.target_zhistory.insert(0, zpredict_m)
+    
+
+
+    #uncomment to show tracking of target
+    
+    chamfmin_img_row = self.yellow_img_xypos[1] - zpredict_p
+    chamfmin_img_col = self.yellow_img_xypos[0] + xpredict_p
+    image_orange[(chamfmin_img_row-30):(chamfmin_img_row+30), chamfmin_img_col] = 100
+    image_orange[chamfmin_img_row, (chamfmin_img_col-30):(chamfmin_img_col+30)] = 100
+    im1=cv2.imshow('window1', image_orange)
+    cv2.waitKey(1)
+    
+
+    return np.array([xpredict_m, zpredict_m])
+
+
   # Recieve data, process it, and publish
   def callback2(self,data):
+
+    # first time the node is run callback receives a faulty image from the camera
+    if not self.callback_called:
+      self.callback_called = True
+      return
+
+
     # Recieve the image
     try:
       cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -123,15 +248,37 @@ class image_converter:
       print(e)
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
+    
+    # Uncomment to show the feed from the camera
+    #im2=cv2.imshow('window2',cv_image2)
+    #cv2.waitKey(1)
+    
+
+    # Calculate the position of the yellow joint in the image once
+    if np.all(self.yellow_img_xypos == np.array([-1, -1])):
+      self.yellow_img_xypos = self.detect_yellow2(cv_image2)
+
+    # Calculate the position of the blue joint in the image once
+    if np.all(self.blue_img_xypos == np.array([-1, -1])):
+      self.blue_img_xypos = self.detect_blue2(cv_image2)
+
+    # Calculate the pixel/meter ratio once
+    if self.pixel_meter_ratio == 0:
+      self.pixel_meter_ratio = np.linalg.norm(self.yellow_img_xypos - self.blue_img_xypos)/2
+      print(self.pixel_meter_ratio)
+
+
+    # Detect the target's (x, z) position
+    target_xzposition = self.detect_target(cv_image2)
+
+
+
 
     d = self.detect_joint_positions2(cv_image2)
-
- 
-    #im2=cv2.imshow('window2',cv_image2)
-    cv2.waitKey(1)
-
     self.positions2 = Float64MultiArray()
     self.positions2.data=d
+    
+
     
 
 
@@ -140,6 +287,10 @@ class image_converter:
       self.image_pub2.publish(self.bridge.cv2_to_imgmsg(cv_image2, "bgr8"))
       self.positions_pub2.publish(self.positions2) 
       # print(self.positions2.data)
+
+      # publish the estimated position of the target
+      self.target_xpredict_pub.publish(target_xzposition[0])
+      self.target_zpredict2_pub.publish(target_xzposition[1])
 
       with open('positions2.txt', 'wb') as fp:
         pickle.dump(self.positions2, fp)
